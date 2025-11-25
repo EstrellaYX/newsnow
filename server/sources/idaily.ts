255e746ba9644c8ee52677b5cf9f57bac72625d5
EstrellaYX <wuyuexin22@gmail.com>
2025-11-25 11:35:10 +0800

    Update idaily.ts

--- server/sources/idaily.ts
+++ server/sources/idaily.ts
@@ -1,30 +1,34 @@
 import { defineSource } from "#/utils/source"
+import { myFetch } from "#/utils/request"
 
-// iDaily 结构
 interface IDailyItem {
   id: number
   title: string
   link_share: string
-  pubdate_timestamp: number // 它是秒级时间戳
+  pubdate_timestamp: number
   cover_thumb: string
   content: string
 }
 
-export default defineSource(async () => {
-  // iDaily iPhone 接口
-  const url = "http://idaily-app.com/api/list/v3/iphone/zh-hans?page=1"
-  
-  const rawData = await myFetch(url) as IDailyItem[]
+interface IDailyResponse {
+  list: IDailyItem[]
+}
 
-  return rawData.map(item => ({
+export default defineSource(async () => {
+  // 用你的 Vercel API 来反代 HTTP 接口
+  const url = "/api/idaily"
+
+  const rawData = await myFetch(url) as IDailyResponse
+
+  const list = rawData.list ?? []
+
+  return list.map(item => ({
     id: item.id.toString(),
     title: item.title,
     url: item.link_share,
-    // iDaily 给的是秒，NewsNow 需要毫秒，所以 * 1000
-    pubDate: item.pubdate_timestamp * 1000, 
+    pubDate: item.pubdate_timestamp * 1000,
     extra: {
-        // iDaily 的图片很精美，可以尝试放入 extra (虽然 NewsNow 列表不一定显示)
-        img: item.cover_thumb 
+      img: item.cover_thumb
     }
   }))
 })
